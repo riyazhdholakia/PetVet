7944bb6ffc63246daf376de06b08f2348cac61a9
Riyazh D <riyazh1000@yahoo.com>
2019-04-12 17:56:13 -0400

    Initial Push

---
+++ PetGo/PetGo/SignUpOrLoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PetGo
+//
+//  Created by Riyazh Dholakia on 4/12/19.
+//  Copyright © 2019 Riyazh Dholakia. All rights reserved.
+//
+
+import UIKit
+
+class SignUpOrLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
