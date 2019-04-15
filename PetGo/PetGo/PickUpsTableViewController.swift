c7c1a5e7b7520be7ca1ce1ad83309571afa23438
Riyazh D <riyazh1000@yahoo.com>
2019-04-15 19:10:42 -0400

    Firebase Added

---
+++ PetGo/PetGo/PickUpsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PickUpsTableViewController.swift
+//  PetGo
+//
+//  Created by Riyazh Dholakia on 4/15/19.
+//  Copyright © 2019 Riyazh Dholakia. All rights reserved.
+//
+
+import UIKit
+
+class PickUpsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ PetGo/PetGo/ProfileTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileTableViewController.swift
+//  PetGo
+//
+//  Created by Riyazh Dholakia on 4/15/19.
+//  Copyright © 2019 Riyazh Dholakia. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
